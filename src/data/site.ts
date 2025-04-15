7880716dc957503292965cb6ef8298f2ee3ec888
Yanuar Wanda <yanuar.wanda2@gmail.com>
2025-04-15 21:30:05 +0700

    feat: add timeline and skills to about page

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,4 +1,4 @@
-import type { SiteConfig } from '../types';
+import type {SiteConfig, TimelineProps} from '../types';
 
 export const SITE_CONFIG: SiteConfig = {
   title: "Yanuar Wanda",
@@ -14,3 +14,25 @@ export const SITE_CONFIG: SiteConfig = {
   ],
   theme: "light"
 };
+
+export const experiences: TimelineProps[] = [
+  {
+    title: "Web & Chatbot Developer (Internship)",
+    subtitle: "Radya Digital • Aug 2021 - Nov 2021"
+  },
+  {
+    title: "Web Developer",
+    subtitle: "Radya Digital • Nov 2021 - Nov 2023"
+  }
+]
+
+export const educations: TimelineProps[] = [
+  {
+    title: "Bachelor of Informatics Engineering",
+    subtitle: "UNIKOM Indonesia • 2025"
+  }
+]
+
+export const skills: string[] = [
+  "PHP", "Laravel", "Blade", "Javascript", "Typescript", "React", "Next.js", "C#", ".NET", "REST Api", "Relational Databases", "CI/CD"
+]
