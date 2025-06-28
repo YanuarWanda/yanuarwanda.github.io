5df4726e3ec901e952c932bbb1a9023560b80bbc
Yanuar Wanda <yanuar.wanda2@gmail.com>
2025-06-28 20:38:59 +0700

    feat: added project pages

--- src/data/site.ts
+++ src/data/site.ts
@@ -52,6 +52,23 @@ export const educations: TimelineProps[] = [
   }
 ]
 
+export const projects: TimelineProps[] = [
+  {
+    title: "Auctioneer",
+    subtitle: "Document automation platform for project bidding letters",
+    url: "/projects/auctioneer"
+  },
+  {
+    title: "eBAST",
+    subtitle: "Platform for generating project handover reports (BAST)",
+    url: "/projects/ebast"
+  },
+  {
+    title: "yanuarwanda.github.io",
+    subtitle: "My personal website"
+  }
+]
+
 export const skills: string[] = [
   "PHP", "Laravel", "Blade", "Javascript", "Typescript", "React", "Next.js", "C#", ".NET", "REST Api", "Relational Databases", "CI/CD"
 ]
